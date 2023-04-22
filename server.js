6a7a8fb86393a94496dfe7180b648283470dfca1
Gitmackan <marcus@gameplan.com>
2023-04-22 18:29:56 +0200

    Wrap database connection in async function

--- server.js
+++ server.js
@@ -3,22 +3,24 @@ const app = express();
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-  await mongoose.connect(process.env.DATABASE_URL);
-
-  const db = mongoose.connection;
-  db.on("error", (error) => console.error(error));
-  db.once("open", () => console.log("Connected to database"));
-
-  app.use(express.json());
-
-  app.get("/", (req, res) => {
-    res.send("Api is running");
-  });
-
-  const usersRouter = require("./routes/users");
-  app.use("/users", usersRouter);
-
-  app.listen(process.env.PORT || 8080, () => console.log("Server has started"));
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 connectToDatabase();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+app.listen(process.env.PORT || 8080, () => console.log("Server has started"));

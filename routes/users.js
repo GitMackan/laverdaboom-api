cd59d0e9f7d1b48062074079abfa61e484061350
Gitmackan <marcus@gameplan.com>
2023-04-22 17:15:52 +0200

    change from ts to js

---
+++ routes/users.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const user = require("../models/user");
+const router = express.Router();
+const User = require("../models/user");
+
+// Get all
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get one
+router.get("/:id", getUser, (req, res) => {
+  res.json(res.user);
+});
+
+// Create one
+router.post("/", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Update
+router.patch("/:id", getUser, async (req, res) => {
+  if (req.body.username != null) {
+    res.user.username = req.body.username;
+  }
+  if (req.body.password != null) {
+    res.user.password = req.body.password;
+  }
+
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete one
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    res.json(deletedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (!user) {
+      return send.status(404).json({ message: "Cant find user" });
+    }
+  } catch (error) {
+    return send.status(500).json({ message: error.message });
+  }
+
+  res.user = user;
+  next();
+}
+
+module.exports = router;

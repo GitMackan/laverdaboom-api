e7b933524485b02a19beaa0a50321a7b7b85a5a5
Gitmackan <marcus@gameplan.com>
2024-03-16 18:15:51 +0100

    change email service to correct email

--- routes/email.js
+++ routes/email.js
@@ -3,36 +3,36 @@ const router = express.Router();
 const nodemailer = require("nodemailer");
 
 router.post("/", async (req, res) => {
-  const { name, email, message } = req.body;
+	const { name, email, message } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "laverdaboom@gmail.com",
-      pass: "jjyumkoqzyyqjnwc",
-    },
-  });
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: "laverdaboom@gmail.com",
+			pass: "jjyumkoqzyyqjnwc",
+		},
+	});
 
-  const mailOptions = {
-    from: "laverdaboom@gmail.com",
-    to: "laverdaboom@gmail.com",
-    subject: "New Contact Form Submission",
-    text: `
+	const mailOptions = {
+		from: "laverdaboom@gmail.com",
+		to: "sandra.brannstrom@hotmail.com",
+		subject: "Meddelande från hemsida",
+		text: `
             Name: ${name}
             Email: ${email}
             Message: ${message}
         `,
-  };
+	};
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email: ", error);
-      res.status(500).json({ error: "Failed to send email" });
-    } else {
-      console.log("Email sent:", info.response);
-      res.json({ message: "Email sent successfully" });
-    }
-  });
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.error("Error sending email: ", error);
+			res.status(500).json({ error: "Failed to send email" });
+		} else {
+			console.log("Email sent:", info.response);
+			res.json({ message: "Email sent successfully" });
+		}
+	});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -22,42 +22,42 @@ const compression = require("compression");
 // require("dotenv").config();
 
 const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URL);
-    console.log("Connected to database");
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(process.env.DATABASE_URL);
+		console.log("Connected to database");
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 connectToDatabase();
 
 app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://laverdaboom.com"],
-    credentials: true,
-  })
+	cors({
+		origin: ["http://localhost:3000", "https://laverdaboom.com"],
+		credentials: true,
+	})
 );
 app.use((req, res, next) => {
-  const allowedOrigins = ["http://localhost:3000", "https://laverdaboom.com"];
+	const allowedOrigins = ["http://localhost:3000", "https://laverdaboom.com"];
 
-  const origin = req.headers.origin;
+	const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+	if (allowedOrigins.includes(origin)) {
+		res.header("Access-Control-Allow-Origin", origin);
+	}
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-PINGOTHER"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-  );
-  next();
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization, X-PINGOTHER"
+	);
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+	);
+	next();
 });
 app.use(express.json());
 app.use(cookieParser());
@@ -65,7 +65,7 @@ app.use(compression());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Api is running");
+	res.send("Api is running");
 });
 
 const usersRouter = require("./routes/users");
